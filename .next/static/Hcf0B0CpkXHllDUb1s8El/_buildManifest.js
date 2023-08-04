b69dc9b94e731f1bb033e64fc010b929e16a07dc
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-08-04 02:27:31 -0700

    added calendly in contact form

--- .next/static/Hcf0B0CpkXHllDUb1s8El/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-da4c6173b06d5c95.js","static/chunks/pages/index-893fc2332edecfdf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,76 +1,18 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
-
-interface FormData {
-  name: string;
-  email: string;
-  message: string;
-}
+import {FC, memo} from 'react';
 
 const ContactForm: FC = memo(() => {
-  const defaultData = useMemo(
-    () => ({
-      name: '',
-      email: '',
-      message: '',
-    }),
-    [],
-  );
-
-  const [data, setData] = useState<FormData>(defaultData);
-
-  const onChange = useCallback(
-    <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
-
-      const fieldData: Partial<FormData> = {[name]: value};
-
-      setData({...data, ...fieldData});
-    },
-    [data],
-  );
-
-  const handleSendMessage = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      /**
-       * This is a good starting point to wire up your form submission logic
-       * */
-      console.log('Data to send: ', data);
-    },
-    [data],
-  );
-
-  const inputClasses =
-    'bg-indigo-950 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-cyan-600 rounded-md placeholder:text-cyan-100 placeholder:text-sm text-neutral-200 text-sm';
-
   return (
-    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
-      <input
-        autoComplete="email"
-        className={inputClasses}
-        name="email"
-        onChange={onChange}
-        placeholder="Email"
-        required
-        type="email"
-      />
-      <textarea
-        className={inputClasses}
-        maxLength={250}
-        name="message"
-        onChange={onChange}
-        placeholder="Message"
-        required
-        rows={6}
-      />
-      <button
-        aria-label="Submit contact form"
-        className="w-max rounded-full border-2 border-rose-400 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
-        type="submit">
-        Send Message
-      </button>
-    </form>
+    <div className="text-center p-4">
+      <p className="mb-4 text-neutral-200 text-lg">To schedule an event, please click the link below:</p>
+      <a 
+        href="https://calendly.com/sujalthomas1234/30min" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="text-cyan-600 underline hover:text-cyan-800"
+      >
+        Schedule with Calendly
+      </a>
+    </div>
   );
 });
 
